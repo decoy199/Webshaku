0de73c92adf06a694d77c43b5ce563f29645c256
decoy199 <138860145+decoy199@users.noreply.github.com>
2025-04-04 19:08:09 +1000

    adimn

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import AdminAuth from '../components/AdminAuth';
-import AdminPanel from '../components/AdminPanel';
+import AdminAuth from '../../public/AdminAuth';
+import AdminPanel from '../../public/AdminPanel';
 
 function Admin() {
   const [user, setUser] = useState(null);
