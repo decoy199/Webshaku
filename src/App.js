f6a23f3f0bd6e29b061fc8d25e54b268a891cf9d
decoy199 <138860145+decoy199@users.noreply.github.com>
2025-04-04 20:39:24 +1000

    h

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+// App.js
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminPanel from "./AdminPanel";
 import Admin from './pages/Admin';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
